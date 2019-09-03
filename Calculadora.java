7e6d26116fdea90ac0db3b0975ac20a4220d0747
TheDente <dante48ful@gmail.com>
2019-09-02 21:01:25 -0500

    Agregamos el archivo Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+
+ 
+    public static void main(String[] args) {
+        funciones op = new funciones();
+        op.Leer_Numeros();
+        op.Suma();
+        op.Resta();
+        op.Multi();
+        op.Div();
+        op.Modulo();
+    }
+    
+}
